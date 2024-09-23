fa182b4d32888625f80048610447c8ebcb2c2190
Kira-Vishn9 <Kvishn9@gmail.com>
2024-09-23 14:01:22 +0400

    fix: fix test number 8

--- test/specs/scenario_8.e2e.js
+++ test/specs/scenario_8.e2e.js
@@ -6,8 +6,9 @@ describe('Workspace settings - Editing workspace name', () => {
 
     it('should allow the user to edit the workspace name and save the changes', async () => {
         browser.url(`https://trello.com/home`)
-        await $("//a[contains(text(), 'Log in')]").waitForDisplayed();
-        await $("//a[contains(text(), 'Log in')]").click();
+        await $("//a[@data-uuid='MJFtCCgVhXrVl7v9HA7EH_login']").waitForDisplayed();
+        
+        await $("//a[@data-uuid='MJFtCCgVhXrVl7v9HA7EH_login']").click();
 
         await $('#username').waitForDisplayed();
         await $('#username').setValue('krtstgml@gmail.com');
