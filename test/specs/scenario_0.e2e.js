045c1fc41395e2d3b5fbbb73e5fd4220a6188842
Kira-Vishn9 <Kvishn9@gmail.com>
2024-09-24 12:08:13 +0400

    fix: fix test number 0 change locator for log in

--- test/specs/scenario_0.e2e.js
+++ test/specs/scenario_0.e2e.js
@@ -6,8 +6,8 @@ describe('Workspace settings - Editing workspace name', () => {
 
     it('should allow the user to edit the workspace name and save the changes', async () => {
         browser.url(`https://trello.com/home`)
-        await $("//a[@data-uuid='MJFtCCgVhXrVl7v9HA7EH_login']").waitForDisplayed();
-        await $("//a[@data-uuid='MJFtCCgVhXrVl7v9HA7EH_login']").click();
+        await await $('//div[contains(@class, "jnMZCI")]/a[text()="Log in"]').waitForDisplayed();
+        await await $('//div[contains(@class, "jnMZCI")]/a[text()="Log in"]').click();
 
         await $('#username').waitForDisplayed();
         await $('#username').setValue('krtstgml@gmail.com');
