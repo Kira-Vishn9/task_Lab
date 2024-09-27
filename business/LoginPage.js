f66f87227b2e0e6642f631bbd9a4579b7bf73419
Kira-Vishn9 <Kvishn9@gmail.com>
2024-09-27 19:44:45 +0400

    six

--- business/LoginPage.js
+++ business/LoginPage.js
@@ -1,5 +1,5 @@
 import { browser } from '@wdio/globals';
-import BasePage from '../base/BasePage.js';
+import BasePage from '../base/BasePage.js'
 
 class LoginPage extends BasePage {
     async login() {
--- business/SearchPage.js
+++ business/SearchPage.js
@@ -1,5 +1,5 @@
 import { browser } from '@wdio/globals';
-import BasePage from '../base/BasePage.js';
+import BasePage from '../base/BasePage.js'
 
 class SearchPage extends BasePage {
     async open() {
