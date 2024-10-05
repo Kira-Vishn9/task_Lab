dee710f6a751ede549fdd1e2d87781a7fa17aa8b
Kiryl Kozlouski <kiryl_kozlouski@epam.com>
2024-10-05 14:36:42 +0400

    Integrated spec and HTML reporter, excluded generated files

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,7 +1,4 @@
-// const {existsSync, mkdirSync} = require('fs');
-// const allure = require('allure-commandline');
 import { browser } from '@wdio/globals';
-
 
 export const config = {
     runner: 'local',
@@ -16,13 +13,7 @@ export const config = {
             'goog:chromeOptions': {
                 args: ['--headless', '--disable-gpu', '--window-size=1280,800']
             }
-        },
-        // {
-        //     browserName: 'firefox',
-        //     'moz:firefoxOptions': {
-        //         args: ['-headless']
-        //     }
-        // },
+        }
     ],
     logLevel: 'error',
     bail: 0,
@@ -31,11 +22,17 @@ export const config = {
     connectionRetryTimeout: 120000,
     connectionRetryCount: 3,
     framework: 'mocha',
-    reporters: ['spec',
+    reporters: [
+        'spec',
         ['allure', {
             outputDir: 'allure-results',
             disableWebdriverStepsReporting: true,
             disableWebdriverScreenshotsReporting: true,
+        }],
+        ['html-nice', {
+            outputDir: './reports/html',
+            filename: 'report.html',
+            reportTitle: 'Test Report'
         }]
     ],
     mochaOpts: {
@@ -43,25 +40,4 @@ export const config = {
         timeout: 90000,
         retries: 0,
     },
-    // before: function() {
-    //     browser.maximizeWindow();
-    // },
-    // afterTest: async function(test, context, { error }) {
-    //     if (error) {
-    //         const dirPath = './artifacts/screenshots/';
-    //         const fileName = `${test.title}.png`;
-    //         if (!existsSync(dirPath)) mkdirSync(dirPath, { recursive: true });
-    //         await browser.saveScreenshot(dirPath + fileName);
-    //     }
-    // },
-    // onComplete: function() {
-    //     const generation = allure(['generate', 'allure-results', '--clean']);
-    //     return new Promise((resolve, reject) => {
-    //         const timeout = setTimeout(() => reject(new Error('Allure report generation failed')), 5000);
-    //         generation.on('exit', function(exitCode) {
-    //             clearTimeout(timeout);
-    //             exitCode === 0 ? resolve() : reject(new Error('Allure report generation failed'));
-    //         });
-    //     });
-    // }
 };
